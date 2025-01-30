db3536c6db44f7a1a33e5e3a4847dfd3ab1ef560
Christian Kissner <b8kich@gmail.com>
2025-01-29 20:33:35 -0800

    start_button.gd

--- docs/index.service.worker.js
+++ docs/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1738170027|141142462006';
+const CACHE_VERSION = '1738211545|3162990181';
 /** @type {string} */
 const CACHE_PREFIX = 'Dodge the Creeps-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
